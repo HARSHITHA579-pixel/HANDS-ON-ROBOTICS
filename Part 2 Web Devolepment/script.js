9522886c9093e485e4e317bc279aa54113468b88
Harshitha579-pixel <k.harshitha579@gmail.com>
2025-09-18 09:54:24 +0530

    Delete Part 2 Web Devolepment directory

--- Part 2 Web Devolepment/script.js
+++
@@ -1,9 +0,0 @@
- function updateDistance() {
-            fetch('/distance')
-                .then(res => res.text())
-                .then(data => {
-                    document.getElementById("distance").innerText = data;
-                });
-        }
-        setInterval(updateDistance, 500);
-        window.onload = updateDistance;
